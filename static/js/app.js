2b42c6e9e8f560b9f14a4dbf2a12fd7d7ae8a7d6
nika9916 <nikoloz.dalakishvili.1@iliauni.edu.ge>
2023-03-30 00:31:59 +0400

    filter slider

--- static/js/app.js
+++ static/js/app.js
@@ -28,9 +28,80 @@ closeBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("open");
 });
 
-// circle
 
 
 // fetch
 
 
+const videoCardContainer = document.querySelector('.video-container');
+const api_key = "AIzaSyA5WuZavY57gKciawF3sQ0ezrhvXOXbOqE";
+const video_http = "https://www.googleapis.com/youtube/v3/videos?";
+const channel_http = "https://www.googleapis.com/youtube/v3/channels?";
+
+fetch(video_http + new URLSearchParams({
+    key: api_key,
+    part: 'snippet',
+    chart: 'mostPopular',
+    maxResults: 100,
+    regionCode: 'ge'
+}))
+.then(res => res.json())
+.then(data => {
+    data.items.forEach(item => {
+        getChannelIcon(item);
+    })
+})
+.catch(err => console.log(err));
+
+const getChannelIcon = (video_data) => {
+    fetch(channel_http + new URLSearchParams({
+        key: api_key,
+        part: 'snippet',
+        id: video_data.snippet.channelId
+    }))
+    .then(res => res.json())
+    .then(data => {
+        video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+        makeVideoCard(video_data);
+    })
+}
+
+const makeVideoCard = (data) => {
+    videoCardContainer.innerHTML += `
+    <div class="video">
+        <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
+        <div class="content">
+            <img src="${data.channelThumbnail}" class="channel-icon" alt="">
+            <div class="info">
+                <h4 class="title">${data.snippet.title}</h4>
+                <p class="channel-name">${data.snippet.channelTitle}</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+// slider
+
+// const rightBtn = document.getElementById("slideRight")
+// const leftBtn = document.getElementById("slideLeft");
+
+// rightBtn.onclick = function () {
+//     document.getElementById("slide-container").scrollLeft += 20
+// }
+
+
+// filter slider
+
+
+const filterWrapper = document.querySelector(".filter-wrapper");
+const arrowIcons = document.querySelectorAll(".icon i");
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        filterWrapper.scrollLeft += icon.id === "left" ? -350 : 350;
+    })
+})
+
+
+// load more
